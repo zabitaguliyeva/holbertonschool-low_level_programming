f2904420da15caa41d148bf96ddc0251b170fa2b
root <7644@holbertonstudents.com>
2023-10-16 13:09:48 -0700

    d

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/*more headers goes here*/
+/*betty style doc for functon main goes there */
+int main(void)
+{
+  int n;
+
+  srand(time(0));
+  n=rand()-RAND_MAX / 2;
+  /*your code goes there */
+  return (0);
+}
