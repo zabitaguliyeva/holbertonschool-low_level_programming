3a768844c03d5058b04bdd1877f96ba5ce5e5e68
root <7644@holbertonstudents.com>
2023-10-22 03:14:25 -0700

    d

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,20 @@
-#include <stdio.h>
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Zabita Guliyeva
+ */
 
-int main(void){
-   char ch='z';
-   while(putchar<=putchar(z)){
-     putchar()++;
-     return (0);
-     
-   }
+ #include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+  char letter;
+
+  for (letter= 'a'; letter<='z'; letter++)
+    putchar(letter);
+  putchar('\n');
+  return (0);
+}
