b59bfb30d5713eb359db3e7517529e5dab5562cc
root <7644@holbertonstudents.com>
2023-10-27 06:58:15 -0700

    d

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _puts - a function that prints a sentence followed by new line
- * @i: input
+ * @str: input
  * Return: (i)
  */
 void _puts(char *str)
 {
-  puts (str);
+puts(str);
 }
