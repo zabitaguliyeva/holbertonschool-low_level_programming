240309982363e3244f9be643be91e49cf1ecec2d
root <7644@holbertonstudents.com>
2023-10-27 04:26:47 -0700

    d

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,20 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * swap_int - a function that swaps two number
+ * @a: input
+ * @b: input
+ * Return: 0
  */
-int main(void)
+void swap_int(int *a, int *b)
 {
-    int a;
-    int b;
+int tmp;
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+tmp = *a;
+*a = *b;
+*b = tpm;
+
 }
