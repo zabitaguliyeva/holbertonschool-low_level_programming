55846ee697ed389bcb72c6f50428a3982bf4b9df
root <7644@holbertonstudents.com>
2023-10-27 07:53:27 -0700

    d

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,5 +7,18 @@
  */
 void _print_rev(char *s)
 {
-puts(strrev(s));
+int l, i;
+char *begin_ptr, *end_ptr, ch;
+
+l = strlen(s);
+begin_ptr = s;
+end_ptr = s + l - 1;
+for (i = 0; i < (l - 1) / 2; i++)
+{
+ch = *end_ptr;
+*end_ptr = *begin_ptr;
+*begin_ptr = ch;
+begin_ptr++;
+end_ptr--;
+}
 }
