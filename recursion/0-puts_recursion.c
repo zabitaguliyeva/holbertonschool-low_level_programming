3ba58f67c938c2b9ed72fda7a0546444e8af84e2
root <7644@holbertonstudents.com>
2023-10-30 00:16:18 -0700

    d

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -4,10 +4,15 @@
  *
  * Return: Always 0.
  */
- void _puts_recursion(char *s)
- {
-     if (*s == '\0')
-         return;
-         putchar(*s);
-         _puts_recursion(++s);
- }
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return;
+	}
+
+putchar(*s);
+_puts_recursion(++s);
+
+}
