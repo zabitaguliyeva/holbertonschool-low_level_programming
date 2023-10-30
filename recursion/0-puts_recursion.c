eaa61bf8ba3a51193e03701a172672cd9d1e279c
root <7644@holbertonstudents.com>
2023-10-29 23:53:26 -0700

    d

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints string
+ *
+ * Return: Always 0.
+ */
+ void _puts_recursion(char *s)
+ {
+     if (*s == '\0')
+         return;
+         putchar(*s);
+         _puts_recursion(++s);
+ }
