fd2bd223a15abea25f54b0de2b25c53932b50b85
root <7644@holbertonstudents.com>
2023-10-30 05:12:54 -0700

    d

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a function that calculates power of numbers
+ * @x: base
+ * @y: pow
+ * Return: depends on condition
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+         return (1);
+	}
+        else if (y > 0)
+        {
+         return (x * _pow_recursion(x, y - 1));
+        }
+        else
+        return (-1);
+}
+
+
