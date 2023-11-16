ded9ba2ce4bf32f6281b504e3e6f0cd1252e35e7
zabitaguliyeva <7644@holbertonstudents.com>
2023-11-16 08:04:53 -0800

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		matrix[i] = malloc(width, sizeof(int));
+		matrix[i] = malloc(width * sizeof(int));
 		if (matrix[i] == NULL)
 		{
 			while (i >= 0)
