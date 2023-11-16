15471463466c60dd1fb8b80b1b7adae808116949
zabitaguliyeva <7644@holbertonstudents.com>
2023-11-16 06:24:43 -0800

    d

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *z;
+	int i;
+	int size = strlen(str);
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+
+
+	z = malloc(sizeof(char) * size + 1);
+
+	if (z == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		z[i] = str[i];
+
+	return (z);
+}
