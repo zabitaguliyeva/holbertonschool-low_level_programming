4b5e4d8b41800828283dacaaee24ea4622cbbc0a
zabitaguliyeva <7644@holbertonstudents.com>
2023-11-19 09:58:33 -0800

    h

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- program that prints the name of the file it was compiled from, followed by a new line.
+ * Return:0
+ */
+int main(void)
+{
+	printf(_FILE_);
+	printf("\n");
+	return(0);
+}
