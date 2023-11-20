b620993832bc4a2def26823791b4e374c05f8afd
zabitaguliyeva <7644@holbertonstudents.com>
2023-11-20 05:46:49 -0800

    d

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+
 /*
- *
+ * struct dog: struct
+ * Description: description
  */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
+#endif
