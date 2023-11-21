a9535b53a9d0deba41ae3af0794b9fc8d824596f
zabitaguliyeva <7644@holbertonstudents.com>
2023-11-21 00:12:34 -0800

    f

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees a dog
+ * @d: dog for free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
