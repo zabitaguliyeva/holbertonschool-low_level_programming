20b3f0f406f5193b73a5456adf515e4fee7c36c8
zabitaguliyeva <7644@holbertonstudents.com>
2023-11-22 03:55:55 -0800

    s

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -8,9 +8,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
 	if (array && size)
-	for (i = 0; i < (int)size; i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
