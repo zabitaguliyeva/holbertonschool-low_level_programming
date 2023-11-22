a50d4e45277721948780f557d0a17b9bc5483d1e
zabitaguliyeva <7644@holbertonstudents.com>
2023-11-22 05:02:25 -0800

    s

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function to find indexs which are int
+ * @array: array
+ * @size: size
+ * @cmp: for checking
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || cmp == NULL || array == NULL)
+		return (-1);
+	if (array && size && cmp)
+	for (i = 0; i <= size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
