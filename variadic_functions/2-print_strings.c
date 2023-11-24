2bb5552cdeb6cbb2d73c678b657f32d89f7df380
zabitaguliyeva <7644@holbertonstudents.com>
2023-11-24 07:19:14 -0800

    d

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,6 +13,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
+		if (list == NULL)
+		{
+			printf("(nil)");
+		}
 		printf("%s", va_arg(list, char *));
 				if (i != (n - 1) && separator != NULL)
 				{
