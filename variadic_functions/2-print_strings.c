03cb1caceda4bf8a52eef66f4b09145a0629cf22
zabitaguliyeva <7644@holbertonstudents.com>
2023-11-24 07:10:36 -0800

    d

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - for printing strings
+ * @separator: separator
+ * @n: count of nums
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(list, char *));
+				if (i != (n - 1) && separator != NULL)
+				{
+					printf("%s", separator);
+				}
+				va_end(list);
+	}
+}
+
