a2705bee0b11b7b89a58cfe5b8d66082ef9482f0
zabitaguliyeva <7644@holbertonstudents.com>
2023-12-04 08:19:39 -0800

    Betty coding style errors was corrected

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,14 +14,14 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 		{
-			printf("[%d] %s\n",h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		length++;
 	}
-	return(length);
+	return (length);
 }
