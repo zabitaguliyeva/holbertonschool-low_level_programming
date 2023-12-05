6dd3a566ef8fdb4581cbaccb58fa7ca8959b9666
zabitaguliyeva <7644@holbertonstudents.com>
2023-12-05 08:26:03 -0800

    k

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -33,14 +33,14 @@ list_t *add_node(list_t **head, const char *str)
 	if (copy == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
-		copy[i] = str[i]
-	new = malloc (sizeof(list_t))
+		copy[i] = str[i];
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
-		free(content);
+		free(copy);
 		return (NULL);
 	}
-	new->str = content;
+	new->str = copy;
 	new->len = len;
 	new->next = *head;
 	*head = new;
