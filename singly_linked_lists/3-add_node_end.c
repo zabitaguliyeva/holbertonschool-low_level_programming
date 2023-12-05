b68f0d1d09e441559e37fcb49b063a9c20175b1a
zabitaguliyeva <7644@holbertonstudents.com>
2023-12-05 08:38:22 -0800

    d

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,32 +9,5 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t  *new, *ptr;
-	unsigned int len = 0;
 
-	new = malloc(sizeof(list_t));
-	if (new)
-	{
-		while (*(str + len))
-			len++;
-		new->str = strdup(str);
-		if (new->str)
-		{
-			new->len = len;
-			new->next = NULL;
-			if (!*head)
-			{
-				*head = new;
-				return (new);
-			}
-			ptr = *head;
-			while (ptr->next)
-				ptr = ptr->next;
-			ptr->next = new;
-			return (new);
-		}
-		free(new);
-		return (NULL);
-	}
-	return (NULL);
 }
