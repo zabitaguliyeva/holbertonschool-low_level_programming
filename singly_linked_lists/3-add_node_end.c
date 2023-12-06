9847fff90b160452b79154b6a7255031a0761d25
zabitaguliyeva <7644@holbertonstudents.com>
2023-12-06 05:08:39 -0800

    Betty coding style errors was corrected

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -6,15 +6,19 @@
   * @head: current place in the list
   * @str: string to add to th
   *
+  * Return: new node
   */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *temporary;
+
 	new_node = malloc(sizeof(size_t));
 	if (head == NULL || str == NULL)
 	{
 		return (NULL);
 	}
+	if (new_node == NULL)
+		return (NULL);
 	new_node->len = strlen(str);
 	new_node->str = strdup(str);
 	new_node->next = NULL;
