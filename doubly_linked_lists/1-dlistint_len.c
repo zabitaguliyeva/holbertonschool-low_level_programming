3fbb7e5cee56618cab5401b57aede171a7d2873c
zabitaguliyeva <7644@holbertonstudents.com>
2023-12-06 23:57:02 -0800

    Kernel Style Code

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ * dlistint_len -  a function that returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to doubly linked list
  *
- *
+ * Description: count number of elements in doubly
+ * Return: total number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
 {
@@ -13,5 +16,5 @@ size_t dlistint_len(const dlistint_t *h)
 		h = h->next;
 		count++;
 	}
-	return(count);
+	return (count);
 }
