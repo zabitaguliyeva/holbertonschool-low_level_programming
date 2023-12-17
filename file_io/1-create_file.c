c723e1622350d4c429674de4e170d7fd43802b3d
zabitaguliyeva <7644@holbertonstudents.com>
2023-12-17 04:34:41 -0800

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,13 +4,14 @@
 
 /**
  * create_file - function that creates a file.
- * @filename - variable pointer
+ * @filename: variable pointer
  * @text_content: content file
  * Return: 1 or -1
  */
 int create_file(const char *filename, char *text_content)
 {
 	int i = 0, file;
+
 	if (filename == NULL)
 		return (-1);
 
