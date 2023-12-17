38155edf22d4d95237e08784f75f4ca24cc2ca4a
zabitaguliyeva <7644@holbertonstudents.com>
2023-12-17 05:39:58 -0800

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -6,7 +6,7 @@
 * append_text_to_file - that appends text at the end of a file
 * @filename: variable pointer
 * @text_content: content file
-* Return : 1 or -1
+* Return: 1 or -1
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
