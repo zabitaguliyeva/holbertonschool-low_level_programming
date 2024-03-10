a3c2e7029836850346d3efaab3f124c7b6531f39
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-10 12:07:49 +0000

    a program that prints all possible different combinations of three digits.

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 /*
- * main: Program that prints all possible different combinations of two digits.
+ * main- Program that prints all possible different combinations of two digits.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j;
+	int i;
+	int j;
 	for (i = '0'; i <= '8'; i++)
 	{
 		for (j = i; j <= '9'; j++)
 		{
-			if(i != j)
+			if (i != j)
 			{
 			putchar(i);
 			putchar(j);
-			if(!(i == '8' && j == '9'))
+			if (!(i == '8' && j == '9'))
 			{
 				putchar(',');
 				putchar(' ');
 			}
-			
-			if(i == '8' && j == '9')
+			if (i == '8' && j == '9')
 			{
 				putchar('\n');
 			}
---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*
+ * main- Program that prints all possible different combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i <= '7'; i++)
+	{
+		for (j = i + 1; j <= '8'; j++)
+		{
+			for (k = j + 1; k <= '9'; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (!(i == '7' && j == '8' && k == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else
+				{
+					putchar('\n');
+				}
+
+			}	
+
+		}
+	}
+	return (0);
+}
+
